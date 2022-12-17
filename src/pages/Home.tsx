3c4b1b62d1146c65841ea35165e29df4ff8be7a6
walberson <walberson.mv@gmail.com>
2022-12-16 21:48:00 -0300

    link para paginas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { Header } from "../components/Header";
-
+import { Link } from "react-router-dom";
 export function Home() {
   return (
     <div>
       <Header />
-      <h1>Página inicial</h1>
+      <Link to="/">
+        <button>Link para Home</button>
+      </Link>
+      <Link to="/profile">
+        <button>Link para Profile</button>
+      </Link>
+      <Link to="/profile/walberson">
+        <button>Link para Walberson</button>
+      </Link>
     </div>
   );
 }
